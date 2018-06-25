c34b43bd8fe9fb04372a0bf0aceea1059540d8c3
Lewis Rodgers <lrodgers04@gmail>
2018-06-25 06:18:18 -0700

    Initial commit of TDD by example directory

---
+++ kentbeck-tdd-by-example/wasrun.py
@@ -0,0 +1,31 @@
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+
+    def run(self):
+        exec("self." + self.name + "()")
+
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        TestCase.__init__(self, name)
+
+    def testMethod(self):
+        self.wasRun = 1
+
+
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
+
+
+# test = WasRun("testMethod")
+# print(test.wasRun)
+# test.run()
+# print(test.wasRun)
+
+TestCaseTest("testRunning").run()
